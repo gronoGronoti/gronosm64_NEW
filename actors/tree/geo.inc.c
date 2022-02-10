c5decacf38b160a94691fbb7388a7378efdaab3e
gronoGronoti <74327469+gronoGronoti@users.noreply.github.com>
2022-02-11 00:04:32 +0100

    textures

--- actors/tree/geo.inc.c
+++ actors/tree/geo.inc.c
@@ -1,71 +1,66 @@
-// 0x16000FE8
+/********************************************************************************
+		Hms tree data.
+														[ Jan 25, 1996 ]
+*********************************************************************************/
+
+extern Gfx  RCP_main_tree[];
+extern Gfx 	RCP_tree02[];
+extern Gfx 	RCP_tree03[];
+extern Gfx 	RCP_tree04[];
+extern Gfx 	RCP_tree05[];
+
+
+/********************************************************************************/
+/*	const GeoLayout map data of MainTree.												*/
+/********************************************************************************/
 const GeoLayout bubbly_tree_geo[] = {
-   GEO_CULLING_RADIUS(800),
-   GEO_OPEN_NODE(),
-#ifdef OBJ_OPACITY_BY_CAM_DIST
-      GEO_ASM(GEO_TRANSPARENCY_MODE_INTER, geo_update_layer_transparency),
-      GEO_SWITCH_CASE(2, geo_switch_anim_state),
-      GEO_OPEN_NODE(),
-#endif
-         GEO_DISPLAY_LIST(LAYER_ALPHA, tree_seg3_dl_bubbly),
-#ifdef OBJ_OPACITY_BY_CAM_DIST
-         GEO_DISPLAY_LIST(LAYER_TRANSPARENT_INTER, tree_seg3_dl_bubbly_transparent),
-      GEO_CLOSE_NODE(),
-#endif
-   GEO_CLOSE_NODE(),
-   GEO_END(),
+	GEO_CULLING_RADIUS(800),
+	GEO_OPEN_NODE(),
+		GEO_DISPLAY_LIST(LAYER_ALPHA, RCP_main_tree),
+	GEO_CLOSE_NODE(),
+  	GEO_END(),
 };
 
-// 0x16001000
+/********************************************************************************/
+/*	const GeoLayout map data of Tree02.												*/
+/********************************************************************************/
 const GeoLayout spiky_tree_geo[] = {
-   GEO_CULLING_RADIUS(800),
-   GEO_OPEN_NODE(),
-#ifdef OBJ_OPACITY_BY_CAM_DIST
-      GEO_ASM(GEO_TRANSPARENCY_MODE_INTER, geo_update_layer_transparency),
-      GEO_SWITCH_CASE(2, geo_switch_anim_state),
-      GEO_OPEN_NODE(),
-#endif
-         GEO_DISPLAY_LIST(LAYER_ALPHA, tree_seg3_dl_spiky),
-#ifdef OBJ_OPACITY_BY_CAM_DIST
-         GEO_DISPLAY_LIST(LAYER_TRANSPARENT_INTER, tree_seg3_dl_spiky_transparent),
-      GEO_CLOSE_NODE(),
-#endif
-   GEO_CLOSE_NODE(),
-   GEO_END(),
+	GEO_CULLING_RADIUS(800),
+	GEO_OPEN_NODE(),
+		GEO_DISPLAY_LIST(LAYER_ALPHA, RCP_tree02),
+	GEO_CLOSE_NODE(),
+  	GEO_END(),
 };
 
-// 0x16001018
+/********************************************************************************/
+/*	const GeoLayout map data of Tree03.												*/
+/********************************************************************************/
 const GeoLayout snow_tree_geo[] = {
-   GEO_CULLING_RADIUS(800),
-   GEO_OPEN_NODE(),
-#ifdef OBJ_OPACITY_BY_CAM_DIST
-      GEO_ASM(GEO_TRANSPARENCY_MODE_INTER, geo_update_layer_transparency),
-      GEO_SWITCH_CASE(2, geo_switch_anim_state),
-      GEO_OPEN_NODE(),
-#endif
-         GEO_DISPLAY_LIST(LAYER_ALPHA, tree_seg3_dl_snowy_pine),
-#ifdef OBJ_OPACITY_BY_CAM_DIST
-         GEO_DISPLAY_LIST(LAYER_TRANSPARENT_INTER, tree_seg3_dl_snowy_pine_transparent),
-      GEO_CLOSE_NODE(),
-#endif
-   GEO_CLOSE_NODE(),
-   GEO_END(),
+	GEO_CULLING_RADIUS(800),
+	GEO_OPEN_NODE(),
+		GEO_DISPLAY_LIST(LAYER_ALPHA, RCP_tree03),
+	GEO_CLOSE_NODE(),
+  	GEO_END(),
 };
 
-// 0x16001048
+/********************************************************************************/
+/*	const GeoLayout map data of Tree04.												*/
+/********************************************************************************/
+const GeoLayout spiky_tree1_geo[] = {
+	GEO_CULLING_RADIUS(800),
+	GEO_OPEN_NODE(),
+		GEO_DISPLAY_LIST(LAYER_ALPHA, RCP_tree04),
+	GEO_CLOSE_NODE(),
+  	GEO_END(),
+};
+
+/********************************************************************************/
+/*	const GeoLayout map data of Tree05.												*/
+/********************************************************************************/
 const GeoLayout palm_tree_geo[] = {
-   GEO_CULLING_RADIUS(800),
-   GEO_OPEN_NODE(),
-#ifdef OBJ_OPACITY_BY_CAM_DIST
-      GEO_ASM(GEO_TRANSPARENCY_MODE_INTER, geo_update_layer_transparency),
-      GEO_SWITCH_CASE(2, geo_switch_anim_state),
-      GEO_OPEN_NODE(),
-#endif
-         GEO_DISPLAY_LIST(LAYER_ALPHA, tree_seg3_dl_palm),
-#ifdef OBJ_OPACITY_BY_CAM_DIST
-         GEO_DISPLAY_LIST(LAYER_TRANSPARENT_INTER, tree_seg3_dl_palm_transparent),
-      GEO_CLOSE_NODE(),
-#endif
-   GEO_CLOSE_NODE(),
-   GEO_END(),
+	GEO_CULLING_RADIUS(800),
+	GEO_OPEN_NODE(),
+		GEO_DISPLAY_LIST(LAYER_ALPHA, RCP_tree05),
+	GEO_CLOSE_NODE(),
+  	GEO_END(),
 };
